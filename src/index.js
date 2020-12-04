40296e6aa982ae58d357ce65d4fa37f30c6d82d7
Pedro <pedrovcunha87@gmail.com>
2020-12-04 13:58:51 +1100

    fix jsx element property

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const App = () => {
     const buttonText = { text: 'Click Me'};
     const buttonStyle = {backgroundColor: 'blue', color: 'white'}
     return <div>
-        <label className="label" for="name">Enter name:</label>
+        <label className="label" htmlFor="name">Enter name:</label>
         <input id="name" type="text"/>
         {/* {{}} First curly braces indicates we want to send a javastript variable. The second, The javascript object itself */}
         {/* conventions - always use "" anytime we are using , single quotes for any non-jsx properties*/}
